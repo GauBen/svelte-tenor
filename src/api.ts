c24d198f39255c7a437dbfaba2cd4d92783844e6
GauBen <benaim.gautier@gmail.com>
2021-09-16 09:51:09 +0200

    refactor: improved types

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-export interface GifObject {
+export interface GifObject<Formats extends string = GifFormat> {
   /** Tenor result identifier. */
   id: string
   /** A unix timestamp representing when this post was created. */
@@ -6,7 +6,7 @@ export interface GifObject {
   /** True if this post contains audio (only video formats support audio, the gif image file format can not contain audio information). */
   hasaudio: boolean
   /** An array of dictionaries with {@link GifFormat} as the key and {@link MediaObject} as the value. */
-  media: Array<Record<GifFormat, MediaObject>>
+  media: Array<Record<Formats, MediaObject>>
   /** An array of tags for the post. */
   tags: string[]
   /** The title of the post. */
@@ -61,7 +61,29 @@ export interface ApiError {
   error: string
 }
 
-const endpoint = <Input, Output>(name: string) => async (options: Input) => {
+export interface CommonOptions {
+  /** Client key. You may use `LIVDSRZULELA` for testing. */
+  key: string
+  /** Default language to interpret search string. */
+  locale?: string
+  contentfilter?: 'off' | 'low' | 'medium' | 'high'
+  ar_range?: 'all' | 'wide' | 'standard'
+  limit?: number
+  pos?: string
+  anon_id?: string
+}
+export interface CommonSearchOptions extends CommonOptions {
+  /** A search string. */
+  q: string
+}
+export interface CommonResults<Formats extends string = GifFormat> {
+  next: string
+  results: GifObject<Formats>[]
+}
+
+export const endpoint = <Input, Output>(name: string) => async (
+  options: Input
+): Promise<Output> => {
   const url = new URL(`https://g.tenor.com/v1/${name}`)
   for (const [name, value] of Object.entries(options))
     url.searchParams.set(name, value.toString())
@@ -76,43 +98,46 @@ const endpoint = <Input, Output>(name: string) => async (options: Input) => {
   return response.json() as Promise<Output>
 }
 
-export const trending = endpoint<
-  {
-    /** Client key. You may use `LIVDSRZULELA` for testing. */
-    key: string
-    /** Default language to interpret search string. */
-    locale?: string
-    media_filter?: 'minimal' | 'basic'
-    contentfilter?: 'off' | 'low' | 'medium' | 'high'
-    limit?: number
-    pos?: string
-    anon_id?: string
-  },
-  {
-    next: string
-    results: GifObject[]
-  }
->('trending')
+export const endpoints = {
+  search: endpoint('search'),
+  trending: endpoint('trending'),
+}
 
-export const search = endpoint<
-  {
-    /** Client key. You may use `LIVDSRZULELA` for testing. */
-    key: string
-    /** A search string. */
-    q: string
-    /** Default language to interpret search string. */
-    locale?: string
-    media_filter?: 'minimal' | 'basic'
-    contentfilter?: 'off' | 'low' | 'medium' | 'high'
-    limit?: number
-    pos?: string
-    anon_id?: string
-  },
-  {
-    next: string
-    results: GifObject[]
+export async function search(
+  options: CommonSearchOptions
+): Promise<CommonResults>
+export async function search(
+  options: CommonSearchOptions & {
+    media_filter: 'minimal'
   }
->('search')
+): Promise<CommonResults<'gif' | 'tinygif' | 'mp4'>>
+export async function search(
+  options: CommonSearchOptions & {
+    media_filter: 'basic'
+  }
+): Promise<
+  CommonResults<'gif' | 'tinygif' | 'nanogif' | 'mp4' | 'tinymp4' | 'nanomp4'>
+>
+export async function search(options: unknown): Promise<unknown> {
+  return endpoints.search(options)
+}
+
+export async function trending(options: CommonOptions): Promise<CommonResults>
+export async function trending(
+  options: CommonOptions & {
+    media_filter: 'minimal'
+  }
+): Promise<CommonResults<'gif' | 'tinygif' | 'mp4'>>
+export async function trending(
+  options: CommonOptions & {
+    media_filter: 'basic'
+  }
+): Promise<
+  CommonResults<'gif' | 'tinygif' | 'nanogif' | 'mp4' | 'tinymp4' | 'nanomp4'>
+>
+export async function trending(options: unknown): Promise<unknown> {
+  return endpoints.trending(options)
+}
 
 export const autocomplete = endpoint<
   { key: string; q: string; locale?: string; limit?: number; anon_id?: string },
