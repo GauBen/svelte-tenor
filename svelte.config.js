727af1e41b2a1fa93c96d3da8ee6e2a1e9383639
GauBen <benaim.gautier@gmail.com>
2021-09-19 00:41:27 +0200

    chore: added rollup to build the package

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,6 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+const config = { preprocess: preprocess(), emitCss: false }
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
-
-export default config;
+export default config
