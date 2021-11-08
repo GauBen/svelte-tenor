525782e025ac2c3b69d62e111ac2e7ad20b82f5f
GauBen <benaim.gautier@gmail.com>
2021-11-08 15:31:41 +0100

    style: updated comments

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,3 @@
-// const path = require('path')
-// const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.svelte'],
   addons: [
@@ -8,23 +5,12 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-svelte-csf',
   ],
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
+  webpackFinal: async (config) => {
+    // Update svelte-loader options to preprocess TypeScript and SCSS
     const svelteLoader = config.module.rules.find(
       (r) => r.loader && r.loader.includes('svelte-loader')
     )
     svelteLoader.options = (await import('../svelte.config.js')).default
-
-    // config.resolve.plugins = [
-    //   new TsconfigPathsPlugin({
-    //     configFile: path.resolve(__dirname, '../tsconfig.json'),
-    //   }),
-    // ]
-
-    // Return the altered config
     return config
   },
 }
