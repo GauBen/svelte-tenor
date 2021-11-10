4aa6accfae5a04f12055630914607619938859ba
GauBen <benaim.gautier@gmail.com>
2021-11-10 16:35:19 +0100

    feat: improved exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,11 @@
-export { default } from './Tenor.svelte'
+// Complete keyboard
+export { default as Tenor } from './Tenor.svelte'
+
+// GIF components
+export { default as Search } from './Search.svelte'
+export { default as ShuffledSearch } from './ShuffledSearch.svelte'
+export { default as Trending } from './Trending.svelte'
+
+// Elementary components
+export { default as Gif } from './Gif.svelte'
+export { default as Grid } from './Grid.svelte'
