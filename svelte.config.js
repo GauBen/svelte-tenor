5cd8811bdeee0effe2694b54f66cdf9a99694d59
GauBen <benaim.gautier@gmail.com>
2021-11-11 19:38:47 +0100

    ci: upload the new website

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 
+const dev = process.env.NODE_ENV === 'development'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
     adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/svelte-tenor',
+    },
   },
 }
 
