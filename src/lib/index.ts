edb519d3c606ca7ca74e418f75a155dab7d77241
GauBen <benaim.gautier@gmail.com>
2021-11-12 22:45:41 +0100

    style: export all components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,6 +6,15 @@ export { default as Search } from './Search.svelte'
 export { default as ShuffledSearch } from './ShuffledSearch.svelte'
 export { default as Trending } from './Trending.svelte'
 
+// Textural components
+export { default as Autocomplete } from './Autocomplete.svelte'
+export { default as Related } from './Related.svelte'
+export { default as TrendingTerms } from './TrendingTerms.svelte'
+
+// Other components
+export { default as Categories } from './Categories.svelte'
+
 // Elementary components
 export { default as Gif } from './Gif.svelte'
 export { default as Grid } from './Grid.svelte'
+export { default as Terms } from './Terms.svelte'
